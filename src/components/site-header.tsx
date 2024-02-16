04d544d8c4a37064be61da0f6f90ebcdf5996c60
Sunil G. Gawai <sunilgawai7397@gmail.com>
2024-02-16 18:41:22 +0530

    site header added

---
+++ src/components/site-header.tsx
@@ -0,0 +1,57 @@
+import { Icons } from "@/components/icons";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export function SiteHeader() {
+  return (
+    <header className="bg-white fixed top-0 z-40 w-full border-b">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <Link href="/" className="flex items-center space-x-2">
+          <Icons.logo className="h-6 w-6" />
+          <span className="inline-block font-bold">Eat Fresh</span>
+        </Link>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <Link
+              href={"/menus"}
+              className="flex items-center text-base font-semibold text-muted-foreground mx-4"
+            >
+              Menus
+            </Link>
+            <Link href={"/wishlist"}>
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "outline",
+                })}
+              >
+                <Icons.wishlist className="h-5 w-5" />
+              </div>
+            </Link>
+            <Link href={"/cart"}>
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.cart className="h-5 w-5" />
+              </div>
+            </Link>
+            <Link href={"/account"}>
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.user className="h-5 w-5" />
+              </div>
+            </Link>
+            {/* <ThemeToggle /> */}
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
